6992d34ccd98aa976d69ce21b824dc63f536ded7
Jeff Aigner <jaigner@avaya.com>
2022-10-04 14:13:33 -0700

    adding git.CurrentTag

--- pkg/git/git.go
+++ pkg/git/git.go
@@ -5,3 +5,7 @@ import "github.com/sheik/create/pkg/create"
 func RepoClean() bool {
 	return create.Command("git diff-index --quiet HEAD") == nil
 }
+
+func CurrentTag() string {
+	return create.Output("git describe --tags | sed 's/-/_/g'")
+}
