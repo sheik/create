3b86cd18419290f133dc6b4378260836f49b2782
Jeff Aigner <enumeration@gmail.com>
2022-10-04 20:47:31 -0700

    fix docker

--- pkg/docker/docker.go
+++ pkg/docker/docker.go
@@ -2,10 +2,10 @@ package docker
 
 import (
 	"fmt"
-	"github.com/sheik/create/pkg/create"
+	"github.com/sheik/create/pkg/shell"
 )
 
 func ImageExists(image string) bool {
 	command := fmt.Sprintf(`bash -c "if [[ \"$(docker images -q %s)\" == \"\" ]]; then exit 1; else exit 0; fi"`, image)
-	return create.Command(command) == nil
+	return shell.Exec(command) == nil
 }
